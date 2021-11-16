fe99cd475b15177877ce43d1efa060e8033ccf21
stevensotelo <stevenbetancurt@hotmail.com>
2021-11-16 14:44:49 -0500

    worked in melisa facebook

--- src/melisafb/melisa.py
+++ src/melisafb/melisa.py
@@ -4,27 +4,32 @@ from flask import Flask, request
 app = Flask(__name__)
 
 FOLDER_APP = "/home/hsotelo/melisa/facebook/"
-#FOLDER_APP = "/home/hsotelo/melisafb/"
-#FOLDER_APP = "G:\\Me\\Code\\UOC\\TFM\\demeter\\src\\facebook_client\\"
+#FOLDER_APP = "D:\\Me\\Code\\UOC\\TFM\\demeter\\src\\melisafb\\"
 #FOLDER_APP = "/app/"
 FILE_TOKEN = FOLDER_APP + "token.txt"
 FILE_TOKEN_DEMETER = FOLDER_APP + "token_demeter.txt"
 TOKEN = ""
 MELISA_NAME = "facebook"
 TOKEN_DEMETER = ""
-DEMETER_URL = "https://demeter.aclimatecolombia.org/api/v1/query"
+DEMETER_URL = "https://demeter.aclimate.org/api/v1/query"
 
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Running MelisaBot for Facebook</h1>'''
 
-
 # Adds support for GET requests to our webhook
 @app.route('/webhook',methods=['GET','POST'])
-def webhook():
+def webhook():    
     if request.method == 'GET':
         verify_token = request.args.get("hub.verify_token") 
-        if verify_token == TOKEN:
+        challenge = request.args.get('hub.challenge')
+        mode = request.args.get('hub.mode')
+        print(verify_token," ",challenge," ",mode)
+        if mode and verify_token:
+            if mode == 'subscribe' and verify_token == TOKEN:
+                print("WEBHOOK_VERIFIED")
+                return challenge
+        else:
             # Responds with the challenge token from the request
             return request.args.get("hub.challenge")
         return 'Unable to authorise.'
@@ -38,6 +43,14 @@ def webhook():
                         if message['message'].get('text'):
                             text = message['message'].get('text')
                             url = DEMETER_URL + "?melisa=" + MELISA_NAME + "&token=" + TOKEN_DEMETER + "&user=" + sender_id + "&message=" + text
+                            print(url)
+                            request_body = {
+                                    'recipient': {
+                                        'id': sender_id
+                                    },
+                                    'message': {"text":"Hola, en breve te enviaremos una respuesta"}
+                                }
+                            requests.post('https://graph.facebook.com/v9.0/me/messages?access_token='+TOKEN,json=request_body)
                             requests.get(url, timeout=1)
                         else:
                             request_body = {
@@ -50,6 +63,24 @@ def webhook():
         except Exception:
             return 'ok'
     return 'ok'
+
+"""
+@app.route('/webhook',methods=['GET','POST'])
+def webhook():
+    # Parse the query params
+    mode = request.values['hub.mode']
+    token = request.values['hub.verify_token']
+    challenge = request.values['hub.challenge']
+    print(request.values['hub.mode'])
+    if mode and token:
+        if mode == 'subscribe' and token == TOKEN:
+            print("WEBHOOK_VERIFIED")
+            return challenge
+        else:
+            return 'Unable to authorise.'
+    return 'Unable to authorise.'
+"""
+
 
 @app.route("/receptor", methods=['POST'])
 def receptor():
@@ -75,10 +106,10 @@ if __name__ == "__main__":
     with open(FILE_TOKEN_DEMETER, "r") as f:
         TOKEN_DEMETER = f.read()
     
-    app.run(threaded=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
     #app.run(host='0.0.0.0', port=8080)
     print("Start server on PORT 5000")
 
 # Run in background
 # nohup python3.8 melisa.py > melisa.log 2>&1 &
-# nohup python3 melisa.py > melisa.log 2>&1 &
+# nohup python melisa.py > melisa.log 2>&1 &
